2235922591be636ef384b5a769c461253dee8ac5
Noah8820 <noah.deboni@noseryoung.com>
2023-11-27 13:57:58 +0100

    adding D2 exercise

---
+++ m320_D2/src/main/java/org/example/Flug.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.example.Passagier;
+
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Flug {
+    private List<Passagier> passagiere;
+
+    public Flug() {
+        this.passagiere = new ArrayList<>();
+    }
+
+    public void passagierAdden() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Geben Sie den Namen des Passagiers ein:");
+        String passagierName = sc.nextLine();
+        Passagier passagier = new Passagier(passagierName);
+        passagiere.add(passagier);
+        System.out.println(passagierName + " wurde hinzugefügt.");
+    }
+
+    public void passagierDeleten() {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Geben Sie den Namen des Passagiers ein:");
+        String passagierName = sc1.nextLine();
+        Passagier passagierToDelete = null;
+
+        for (Passagier passagier : passagiere) {
+            if (passagier.getName().equals(passagierName)) {
+                passagierToDelete = passagier;
+                break;
+            }
+        }
+        if (passagierToDelete != null) {
+            passagiere.remove(passagierToDelete);
+            System.out.println("Passagier " + passagierName + " wurde entfernt.");
+        }
+    }
+
+    public void passagierListeAusgeben() {
+        System.out.println("Passagierliste :");
+        for (Passagier p : passagiere) {
+            p.nameAusgeben();
+        }
+    }
+}
---
+++ m320_D2/src/main/java/org/example/Passagier.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Passagier {
+    private String name;
+
+    public Passagier(String name) {
+        this.name = name;
+    }
+
+    public void nameAusgeben() {
+        System.out.println("Passagier: " + name);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
